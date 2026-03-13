0e20544e6c04db6819b6865828f8ea78c989832d
Nitin Gupta <nitin978032@gmail.com>
2026-03-13 20:04:01 +0530

    feat: add method-override package with debug and ms dependencies
    - Introduced method-override package to handle HTTP verb overrides.
    - Added debug package for logging with browser and Node.js support.
    - Included ms package for time duration parsing and formatting.
    - Created Karma configuration for testing the debug package.
    - Implemented browser and Node.js specific logic in debug package.
    - Added necessary files and configurations for method-override and its dependencies.
    - Created edit listing view for user input in the application.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URI = 'mongodb://localhost:27017/wanderlust';
 
@@ -16,6 +17,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get('/', (req, res) => {
     
@@ -54,6 +56,32 @@ app.get('/listings/new', (req, res) => {
   await newListing.save();
   res.redirect("/listings");
 });
+
+//Edit route
+// app.get('/listings/:id/edit', async (req, res) => {
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
+
+
+
 // app.get('/testListing', async (req, res) => {
 //     let newListing = new Listing({
 //         title: "Beautiful Beach House",
