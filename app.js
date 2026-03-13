fc8aed974ec0ad3d43c0da86c4587f1c760ac06c
Student hastagnitin <nitin978032@.com>
2026-03-13 10:53:16 +0530

    get

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require("path");
 
 const MONGO_URI = 'mongodb://localhost:27017/wanderlust';
 
@@ -12,25 +13,33 @@ async function main() {
     await mongoose.connect(MONGO_URI);
 }
 
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get('/', (req, res) => {
     
     res.send('Hello  i am root');
 });
-app.get('/testListing', async (req, res) => {
-    let newListing = new Listing({
-        title: "Beautiful Beach House",
-        description: "A stunning beach house with ocean views.",    
-        price: 250,
-        location: "Malibu, California",
-        country: "USA"
+
+app.get('/listings',  async(req, res) => {
+  const allListings = await Listing.find({});
+  res.render("Listings/index.ejs", { allListings });
+
+    });
+ 
+// app.get('/testListing', async (req, res) => {
+//     let newListing = new Listing({
+//         title: "Beautiful Beach House",
+//         description: "A stunning beach house with ocean views.",    
+//         price: 250,
+//         location: "Malibu, California",
+//         country: "USA"
     
-});
-  await newListing.save();
-  console.log('Sample listing saved to database');
-  res.send('Sample listing created and saved to database');
-});
+// });
+//   await newListing.save();
+//   console.log('Sample listing saved to database');
+//   res.send('Sample listing created and saved to database');
+// });
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
