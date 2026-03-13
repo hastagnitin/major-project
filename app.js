eb6334533924b8ca2a55bd85f93b7e6a34d43b0f
Nitin Gupta <nitin978032@gmail.com>
2026-03-13 12:08:00 +0530

    Remove commented-out code for listing details route in app.js

--- app.js
+++ app.js
@@ -29,11 +29,7 @@ app.get('/listings',  async(req, res) => {
     });
  
     //show rouye
-    // app.get('/listings/:id', async (req, res) => {
-    //     let id = req.params;
-    //    const listing = await Listing.findById(id);
-    //    res.render('Listings/show.ejs', { listing });
-    // });
+    
 
     app.get('/listings/:id', async (req, res) => {
     // Option A: Destructuring (Cleaner)
